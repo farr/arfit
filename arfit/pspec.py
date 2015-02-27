fbb35a4f96d4d56ff3b0fde4227e2dbeefece4b0
Will Meierjurgen Farr <wmfarr@gmail.com>
2015-02-27 21:35:38 +0000

    Added normalised_lombscargle to pspec.py

--- arfit/pspec.py
+++ arfit/pspec.py
@@ -48,3 +48,20 @@ def acf(ts, data):
 
     paddedacf = np.fft.ifft(np.square(np.abs(np.fft.fft(paddedidata))))
     return its-its[0], paddedacf[:idata.shape[0]]/paddedacf[0]
+
+def normalised_lombscargle(ts, ys, fs):
+    """Returns a Lomb-Scargle periodigram, normalised to integrate
+    over frequencies to the variance of the ``ys``. 
+
+    """
+
+    omegas = 2.0*np.pi*fs
+    unnorm_ls = ss.lombscargle(ts, ys, omegas)
+
+    T = ts[-1] - ts[0]
+
+    mu = np.trapz(ys, ts)/T
+
+    var = np.trapz(np.square(ys - mu), ts)/T
+
+    return var * unnorm_ls / np.trapz(unnorm_ls, fs)
