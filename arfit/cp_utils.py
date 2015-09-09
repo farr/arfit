db40a42c2825b157017e6730a2b5c95371bbe598
Will Meierjurgen Farr <wmfarr@gmail.com>
2015-09-09 06:53:54 +0100

    Allow user to adjust nyquist freq and freq spacing in cp_utils.py

--- arfit/cp_utils.py
+++ arfit/cp_utils.py
@@ -14,28 +14,21 @@ def csample_from_files(datafile, chainfile, p, q):
 
     return cm.CarmaSample(data[:,0], data[:,1], data[:,2], None, q=q, trace=chain[:,:-2], loglike=chain[:,-2], logpost=chain[:,-1])
 
-def normalised_lombscargle(ts, ys, dys):
+def normalised_lombscargle(ts, ys, dys, oversampling=5, nyquist_factor=3):
     model = LombScargleFast().fit(ts, ys, dys)
 
-    T = np.max(ts)-np.min(ts)
-    dts = np.diff(np.sort(ts))
+    pers, pows = model.periodogram_auto(oversampling=oversampling, nyquist_factor=nyquist_factor)
+    fs = 1.0/pers
 
-    fny = 1.0/(2.0*np.min(dts))
-    df = 1.0/T
+    T = np.max(ts) - np.min(ts)
 
-    N = fny/df
-
-    fs = np.linspace(df, fny, N)
-
-    pows = model.score_frequency_grid(df, df, N)
-
-    mu = 1.0/T*np.trapz(ys, ts)
-    s2 = 1.0/T*np.trapz(np.square(ys-mu), ts)
+    mu = 1/T*np.trapz(ys, ts)
+    s2 = 1/T*np.trapz(np.square(ys-mu), ts)
 
     return fs, s2*pows/np.trapz(pows, fs)
 
 
-def plot_psd_sample_data(sample):
+def plot_psd_sample_data(sample, oversampling=5, nyquist_factor=3):
     psd_low, psd_high, psd_med, fs = sample.plot_power_spectrum(doShow=False)
 
     plt.clf()
@@ -43,7 +36,7 @@ def plot_psd_sample_data(sample):
     plt.loglog(fs, psd_med, '-b', alpha=0.33)
     plt.fill_between(fs, psd_low, psd_high, color='b', alpha=0.17)
 
-    fs, psd = normalised_lombscargle(sample.time, sample.y, sample.ysig)
+    fs, psd = normalised_lombscargle(sample.time, sample.y, sample.ysig, oversampling=oversampling, nyquist_factor=nyquist_factor)
 
     bw = fs[-1] - fs[0]
     T = sample.time[-1] - sample.time[0]
@@ -56,12 +49,12 @@ def plot_psd_sample_data(sample):
 
     plt.loglog(fs, psd, '-r', alpha=0.33)
 
-def plot_psd_sample_draw(sample, loc='upper left'):
-    fs, psd = normalised_lombscargle(sample.time, sample.y, sample.ysig)
+def plot_psd_sample_draw(sample, loc='upper left', oversampling=5, nyquist_factor=3):
+    fs, psd = normalised_lombscargle(sample.time, sample.y, sample.ysig, oversampling=oversampling, nyquist_factor=nyquist_factor)
 
     ys_draw = sample.predict(sample.time, bestfit='random')[0]
 
-    fs, dpsd = normalised_lombscargle(sample.time, ys_draw, sample.ysig)
+    fs, dpsd = normalised_lombscargle(sample.time, ys_draw, sample.ysig, oversampling=oversampling, nyquist_factor=nyquist_factor)
 
     plt.loglog(fs, psd, '-k', label='Data', alpha=0.5)
     plt.loglog(fs, dpsd, '-b', label='Prediction', alpha=0.5)
