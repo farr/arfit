b1750e48aa89906a3aa4b156d60229c53db07c2b
Will Meierjurgen Farr <wmfarr@gmail.com>
2015-09-25 22:40:44 +0100

    Use the same frequencies for CARMA PSD and LS PSD in utils.plot_psd

--- arfit/utils.py
+++ arfit/utils.py
@@ -11,12 +11,11 @@ import plotutils.runner as pr
 import scipy.stats as ss
 import triangle as tri
 
-def plot_psd(runner, xlabel=None, ylabel=None, Npts=1000, Nmcmc=1000, oversampling=5, nyquist_factor=3):
-    logpost = runner.sampler.logp.lp
-    chain = runner.burnedin_chain[0,...].reshape((-1, runner.chain.shape[3]))
-    
-    fs, ls = ps.normalised_lombscargle(logpost.t, logpost.y, logpost.dy, oversampling=oversampling, nyquist_factor=nyquist_factor)
+def plot_psd(logpost, chain, xlabel=None, ylabel=None, Npts=1000, Nmcmc=1000, oversampling=5, nyquist_factor=3):
+    ls_fs, ls = ps.normalised_lombscargle(logpost.t, logpost.y, logpost.dy, oversampling=oversampling, nyquist_factor=nyquist_factor)
 
+    fs = linspace(np.min(ls_fs), np.max(ls_fs), Npts)
+    
     psds = []
     wns = []
     for p in permutation(chain)[:Nmcmc,:]:
@@ -33,7 +32,7 @@ def plot_psd(runner, xlabel=None, ylabel=None, Npts=1000, Nmcmc=1000, oversampli
     except:
         pass
 
-    loglog(fs, ls, '-k')
+    loglog(ls_fs, ls, '-k')
     
     loglog(fs, median(psds, axis=0), '-b')
     fill_between(fs, percentile(psds, 84, axis=0), percentile(psds, 16, axis=0), color='b', alpha=0.25)
@@ -56,26 +55,23 @@ def plot_psd(runner, xlabel=None, ylabel=None, Npts=1000, Nmcmc=1000, oversampli
     if ylabel is not None:
         plt.ylabel(ylabel)
 
-def plot_residuals(runner, Nmcmc=1000, Npts=1000):
-    logpost = runner.sampler.logp.lp
+def plot_residuals(logpost, chain, Nmcmc=1000, Npts=1000):
     resid = []
-    for p in permutation(runner.burnedin_chain[0,...].reshape((-1, logpost.nparams)))[:Nmcmc, :]:
+    for p in permutation(chain)[:Nmcmc, :]:
         resid.append(logpost.standardised_residuals(p))
     resid = array(resid)
     errorbar(logpost.t, mean(resid, axis=0), yerr=std(resid, axis=0), color='k', fmt='.')
 
-def plot_resid_distribution(runner, N=10):
-    logpost = runner.sampler.logp.lp
-    for p in permutation(runner.burnedin_chain[0,...].reshape((-1, logpost.nparams)))[:N, :]:
+def plot_resid_distribution(logpost, chain, N=10):
+    for p in permutation(chain)[:N, :]:
         r = logpost.standardised_residuals(p)
         pu.plot_kde_posterior(r, color='b', alpha=0.1, label='Resid')
 
     for i in range(N):
         pu.plot_kde_posterior(randn(r.shape[0]), color='k', alpha=0.1, label='N(0,1)')
         
-def plot_resid_acf(runner, N=10):
-    logpost = runner.sampler.logp.lp
-    for p in permutation(runner.burnedin_chain[0,...].reshape((-1, logpost.nparams)))[:N,:]:
+def plot_resid_acf(logpost, chain, N=10):
+    for p in permutation(chain)[:N,:]:
         r = logpost.standardised_residuals(p)
         acorr(r, maxlags=None, alpha=0.1)
 
@@ -102,11 +98,9 @@ def dic(runner):
 
     return -2.0*(np.mean(lnlikes) - np.var(lnlikes))
 
-def residual_pvalues(runner, Nps=10):
-    logpost = runner.sampler.logp.lp
-
+def residual_pvalues(logpost, chain, Nps=10):
     stats = []
-    for p in permutation(runner.burnedin_chain[0,...].reshape((-1, logpost.nparams)))[:Nps,:]:
+    for p in permutation(chain)[:Nps,:]:
         r = logpost.standardised_residuals(p)
         stat, cv, sl = ss.anderson(r, 'norm')
         stats.append(stat)
@@ -114,15 +108,14 @@ def residual_pvalues(runner, Nps=10):
 
     return stats, cv, sl
 
-def plot_prediction(runner, Npred=1000, Nts=1000):
-    logpost = runner.sampler.logp.lp
-
+def plot_prediction(logpost, chain, Npred=100, Nts=1000):
     ts = linspace(np.min(logpost.t), np.max(logpost.t), Nts)
+    ts = np.sort(np.concatenate((ts, logpost.t)))
 
     preds = []
     uls = []
     lls = []
-    for p in permutation(runner.burnedin_chain[0,...].reshape((-1, logpost.nparams)))[:Npred,:]:
+    for p in permutation(chain)[:Npred,:]:
         pre = logpost.predict(p, ts)
         preds.append(pre[0])
         uls.append(pre[0] + sqrt(pre[1]))
@@ -136,20 +129,15 @@ def plot_prediction(runner, Npred=1000, Nts=1000):
         
     errorbar(logpost.t, logpost.y, logpost.dy, color='k', fmt='.')
 
-def plot_simulation(runner, Nsim=10):
-    logpost = runner.sampler.logp.lp
-
-    for p in permutation(runner.burnedin_chain[0,...].reshape((-1, logpost.nparams)))[:Nsim,:]:
-        plot(logpost.t, logpost.simulate(p, logpost.t), '-b', alpha=0.1)
-
-    errorbar(logpost.t, logpost.y, logpost.dy, fmt='.', color='k')                
-    
 def process_output_dir(dir, runner=None, return_runner=False):
     if runner is None:
         with bz2.BZ2File(op.join(dir, 'runner.pkl.bz2'), 'r') as inp:
             runner = pickle.load(inp)
 
     runner.sampler.logp.lp._mean_variance()
+
+    logpost = runner.sampler.logp.lp
+    chain = runner.burnedin_chain[0,...].reshape((-1, logpost.nparams))
 
     figure()
     plot(1/runner.sampler.beta_history.T)
@@ -162,19 +150,19 @@ def process_output_dir(dir, runner=None, return_runner=False):
     savefig(op.join(dir, 'chains0.pdf'))
 
     figure()
-    plot_psd(runner)
+    plot_psd(logpost, chain)
     savefig(op.join(dir, 'psd.pdf'))
 
     figure()
-    plot_residuals(runner)
+    plot_residuals(logpost, chain)
     savefig(op.join(dir, 'resid.pdf'))
 
     figure()
-    plot_resid_distribution(runner)
+    plot_resid_distribution(logpost, chain)
     savefig(op.join(dir, 'resid-distr.pdf'))
 
     figure()
-    plot_resid_acf(runner)
+    plot_resid_acf(logpost, chain)
     savefig(op.join(dir, 'resid-acf.pdf'))
 
     figure()
